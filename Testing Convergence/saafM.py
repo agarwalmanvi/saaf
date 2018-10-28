fb082f24a13c9e5c99ddd3d3837557bbaeff30c2
agarwalmanvi <94.manviagarwal@gmail.com>
2018-10-28 23:19:43 +0100

    Update saafM.py

--- Testing Convergence/saafM.py
+++ Testing Convergence/saafM.py
@@ -69,21 +69,24 @@ class Structure:
 		self.votes_support_relations[(arg1,arg2)] = (pro,con)
 		
 	def randomInit(self):
+		# random values between 0 and 1 for each argument to use in the iterations
 		self.initial_values = [np.random.uniform(0, 1) for i in range(self.var)]
+		# total number of edges needed to get desired density
 		n_edges = math.floor(2 * scipy.special.comb(self.var, 2) * self.density)
+		# keeps track of which nodes still have space for more edges
 		available_nodes = list(range(self.var))
 		for i in range(n_edges):
-#			print("Step: ", i)
-#			print("available nodes: ",available_nodes)
 			u = np.random.choice(available_nodes)
+			# ensures that u does not end up in free_nodes
 			self.attack_relations[u][u] = 1
 			conn_nodes = self.attack_relations[u]
-#			print("All nodes: ", conn_nodes)
+			# nodes that are available to be connected
 			free_nodes = np.where(conn_nodes == 0)
-#			print("Free nodes: ", free_nodes)
+			# if no nodes are available, remove u from available nodes
 			if free_nodes[0].size == 0:
 				available_nodes.remove(u)
-				i += 1
+				i -= 1
+			# if free nodes are available, set value in attack relation adjacency matrix to 1
 			else:
 				v = np.random.choice(free_nodes[0])
 				self.attack_relations[u][v] = 1
@@ -91,26 +94,26 @@ class Structure:
 		n_edges = math.floor(2 * scipy.special.comb(self.var, 2) * self.support_density)
 		available_nodes = list(range(self.var))
 		for i in range(n_edges):
-#			print("Step: ", i)
-#			print("available nodes: ",available_nodes)
 			u = np.random.choice(available_nodes)
 			self.support_relations[u][u] = 1
 			conn_nodes = self.support_relations[u]
-#			print("All nodes: ", conn_nodes)
 			free_nodes = np.where(conn_nodes == 0)
-#			print("Free nodes: ", free_nodes)
 			if free_nodes[0].size == 0:
 				available_nodes.remove(u)
-				i += 1
+				i -= 1
 			else:
 				v = np.random.choice(free_nodes[0])
-				self.support_relations[u][v] = 1
-			self.support_relations[u][u] = 0		
+				if self.attack_relations[u][v] == 0:
+					self.support_relations[u][v] = 1
+			self.support_relations[u][u] = 0	
+		# votes on attack relations
 		indices=list(np.transpose(np.nonzero(self.attack_relations)))
 		for i in range(len(indices)):
 			row, col = indices[i]
 			self.votes_relations[(row, col)] = (np.random.randint(0, 50), np.random.randint(0, 50))
+		# votes on arguments
 		self.votes = list(zip(np.random.randint(0, 50, size=self.var), np.random.randint(0, 50, size=self.var)))
+		# votes on support relations
 		indices=list(np.transpose(np.nonzero(self.support_relations)))
 		for i in range(len(indices)):
 			row, col = indices[i]
@@ -127,17 +130,20 @@ class Structure:
 		votes = self.votes_support_relations[(arg1, arg2)]
 		return votes[0]/(votes[0] + votes[1] + self.epsilon)
 
-def doSaf(s, iters=0):
-	if iters == 0:
-		iters = 10000
+def doSaf(s, iters=10000):
+	# store time for runtime
 	t0 = time.time()
+	# initialise matrix for storing iteration values
 	save_iterations = np.zeros(shape = (iters+1,len(s.initial_values)))
+	# take the initial values for the 0th iteration
 	save_iterations [0,:] = s.initial_values
 	i = 0
 	condition = 0
+	# iterate until convergence as long as you dont run out of iterations
 	while (condition == 0 and i < iters+1):
 		current_iter = copy.deepcopy(save_iterations[i,:])
 		i +=  1
+		# iterative rule
 		for j in range(len(s.initial_values)):        
 			tau = s.votes_eval(j)
 			attacking_set = s.attack_relations[:,j]
@@ -145,8 +151,9 @@ def doSaf(s, iters=0):
 			temp1 = np.subtract(np.ones(s.var),current_iter)
 			temp2 = np.take(temp1,attacking_pos)
 			current_iter[j] = tau*np.prod(temp2)
-
+		# store values in iteration matrix
 		save_iterations[i,:] = current_iter
+		# compare with convergence threshold
 		diff1 = np.subtract(save_iterations[i-1,:],save_iterations[i,:])
 		condition = (np.absolute(diff1) < 0.0000000001).prod()
 		diff2 = np.sum(np.absolute(diff1))
@@ -161,9 +168,7 @@ def doSaf(s, iters=0):
 		s.time = 0
 		s.iterations_needed = 0
 	
-def doEsaf(s, iters=0):
-	if iters == 0:
-		iters = 10000
+def doEsaf(s, iters=10000):
 	t0 = time.time()
 	save_iterations = np.zeros(shape = (iters+1,len(s.initial_values)))
 	save_iterations [0,:] = s.initial_values
@@ -199,9 +204,7 @@ def doEsaf(s, iters=0):
 		s.iterations_needed = 0
 		s.results = str(0)
 
-def doBsaf(s, iters=0):
-	if iters == 0:
-		iters = 10000
+def doBsaf(s, iters=10000):
 	t0 = time.time()
 	save_iterations = np.zeros(shape = (iters+1,len(s.initial_values)))
 	save_iterations [0,:] = s.initial_values
@@ -237,9 +240,7 @@ def doBsaf(s, iters=0):
 		s.iterations_needed = 0
 		s.results = str(0)
 
-def doEbsaf(s, iters=0):
-	if iters == 0:
-		iters = 10000
+def doEbsaf(s, iters=10000):
 	t0 = time.time()
 	save_iterations = np.zeros(shape = (iters+1,len(s.initial_values)))
 	save_iterations [0,:] = s.initial_values
@@ -282,6 +283,7 @@ def doEbsaf(s, iters=0):
 		s.time = 0
 		s.iterations_needed = 0
 		s.results = str(0)
+
 
 
 
